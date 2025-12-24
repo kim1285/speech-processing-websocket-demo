423ac98b6b65745e8d3c7a85b6cf64d47ce802ae
Kangsan Kim <rlarkdtks8713@gmail.com>
2025-12-24 15:15:10 +0900

    chore(set up main.py)

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from contextlib import asynccontextmanager
+from fastapi import APIRouter, FastAPI
+import logging
+from dotenv import load_dotenv
+
+# initialize logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - [%(levelname)s] %(name)s: %(message)s',
+                    handlers=[logging.StreamHandler(),
+                              logging.FileHandler('app.log')
+                              ]
+                    )
+logger = logging.getLogger("app")
+
+# load secretes
+load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # start up tasks
+    # initialize db
+    # create tables
+    # load ml model initially once
+    # instantiate ws connection manager
+    logger.info("Application starting...")
+    # main loop
+    yield
+    # clean up tasks
+    logger.info("Application closing...")
+
+app = FastAPI(lifespan=lifespan)
+
+# register routes
+
